ab856bdd4b78a332eff1fb79cee2d86c0d7de626
6250460 <6250460@cegepmontpetit.ca>
2025-05-02 14:52:47 -0400

    am

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Image docker qa sur environement QA\n');
+    res.end('image docker qa sur environement QA\n');
 });
 
 
